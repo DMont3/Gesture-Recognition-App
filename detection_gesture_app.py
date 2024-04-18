fd05c50dfd522e43603f908bc7d1854102905435
Andre85117 <andremarquessobrinho1609@gmail.com>
2024-04-18 19:55:20 -0300

    New Update 0.1

--- detection_gesture_app.py
+++ detection_gesture_app.py
@@ -1,78 +1,90 @@
 import cv2
-import mediapipe
+import mediapipe as mp
+import pyautogui
 import time
 
-ctime=0
-ptime=0
 
-cap=cv2.VideoCapture(0)
+# Inicializa a captura de vídeo
+cap = cv2.VideoCapture(0)
 
-medhands=mediapipe.solutions.hands
-hands=medhands.Hands(max_num_hands=1,min_detection_confidence=0.7)
-draw=mediapipe.solutions.drawing_utils
 
+# Configurações do MediaPipe Hands
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mp_draw = mp.solutions.drawing_utils
+
+
+# Variável para controlar o tempo desde a última pressão da tecla
+last_space_press_time = 0
+
+
+# Loop principal para captura e processamento de cada frame
 while True:
-    success, img=cap.read() #pega um frame da imagem
-
-    img = cv2.flip(img,1) # inverte a imagem
-
-    imgrgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+   success, img = cap.read()
+   if not success:
+       continue
 
 
-    #realiza a detecção da mão na imagem
-    res = hands.process(imgrgb)
-
-    lmlist=[]
-    tipids=[4,8,12,16,20] # lista com as pontas dos dedos
-
-    #desenha no canto da tela um retangulo, os numeros vão aparecer aqui
-    cv2.rectangle(img,(20,350),(90,440),(0,255,204),cv2.FILLED)
-    cv2.rectangle(img,(20,350),(90,440),(0,0,0),5)
-
-    ## se detectar alguma mão entra no if
-    if res.multi_hand_landmarks:
-        for handlms in res.multi_hand_landmarks:
-            for id,lm in enumerate(handlms.landmark):
-
-                h,w,c= img.shape
-                cx,cy=int(lm.x * w) , int(lm.y * h)
-                lmlist.append([id,cx,cy])
-                if len(lmlist) != 0 and len(lmlist)==21:
-                    fingerlist=[]
-
-                    #thumb and dealing with flipping of hands
-                    if lmlist[12][1] > lmlist[20][1]:
-                        if lmlist[tipids[0]][1] > lmlist[tipids[0]-1][1]:
-                            fingerlist.append(1)
-                        else:
-                            fingerlist.append(0)
-                    else:
-                        if lmlist[tipids[0]][1] < lmlist[tipids[0]-1][1]:
-                            fingerlist.append(1)
-                        else:
-                            fingerlist.append(0)
-
-                    #others
-                    for id in range (1,5):
-                        if lmlist[tipids[id]][2] < lmlist[tipids[id]-2][2]:
-                            fingerlist.append(1)
-                        else:
-                            fingerlist.append(0)
+   img = cv2.flip(img, 1)  # Inverte a imagem
+   img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-                    if len(fingerlist)!=0:  # se a lista for diferente de zero então
-                        fingercount=fingerlist.count(1) # conta quantidade de dedos
+   # Processa a detecção da mão
+   results = hands.process(img_rgb)
 
-                    # escreve na tela a quantidade detectada
-                    cv2.putText(img,str(fingercount),(25,430),cv2.FONT_HERSHEY_PLAIN,6,(0,0,0),5)
 
-                #change color of points and lines
-                draw.draw_landmarks(img,handlms,medhands.HAND_CONNECTIONS,draw.DrawingSpec(color=(0,255,204),thickness=2,circle_radius=2),draw.DrawingSpec(color=(0,0,0),thickness=2,circle_radius=3))
+   if results.multi_hand_landmarks:
+       for hand_landmarks in results.multi_hand_landmarks:
+           # Lista para armazenar a posição dos dedos
+           finger_positions = []
+           for id, lm in enumerate(hand_landmarks.landmark):
+               h, w, c = img.shape
+               cx, cy = int(lm.x * w), int(lm.y * h)
+               finger_positions.append((id, cx, cy))
 
-    cv2.imshow("hand gestures",img)
 
-    #press q to quit
-    if cv2.waitKey(1) == ord('q'):
-        break
+           if finger_positions:
+               # Identificar "Vitória"
+               if (finger_positions[8][2] < finger_positions[6][2]) and (finger_positions[12][2] < finger_positions[10][2]):
+                   gesture = "Vitoria"
+               # Identificar "Legal"
+               elif (finger_positions[4][1] < finger_positions[3][1]) and (finger_positions[20][2] > finger_positions[18][2]):
+                   gesture = "Legal"
+                   current_time = time.time()
+                   # Verifica se já se passaram 5 segundos desde a última pressão da tecla
+                   if current_time - last_space_press_time >= 5:
+                       # Simula a pressão da tecla "espaço"
+                       pyautogui.press('space')
+                       # Atualiza o tempo da última pressão da tecla
+                       last_space_press_time = current_time
+               # Apontar para esquerda ou direita
+               elif (finger_positions[8][2] < finger_positions[6][2]) and all(finger_positions[4*i+2][2] > finger_positions[4*i][2] for i in range(2, 5)):  # Dedo indicador é o único levantado
+                   # Checa se a posição x do dedo indicador é menor que a média das posições x da base dos outros dedos levantados
+                   if finger_positions[8][1] < (finger_positions[5][1] + finger_positions[9][1] + finger_positions[13][1] + finger_positions[17][1]) / 4:
+                       gesture = "Apontar Esquerda"
+                   else:
+                       gesture = "Apontar Direita"
+               else:
+                   gesture = "Nao Definido"
+
+
+               # Desenha o gesto identificado na tela
+               cv2.putText(img, gesture, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+
+           # Desenha as landmarks da mão
+           mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+
+   # Mostra a imagem resultante
+   cv2.imshow("Hand Tracking", img)
+
+
+   # Fecha o programa quando 'q' é pressionado
+   if cv2.waitKey(1) & 0xFF == ord('q'):
+       break
+
+
+# Libera a câmera e destrói todas as janelas abertas
 cap.release()
 cv2.destroyAllWindows()
