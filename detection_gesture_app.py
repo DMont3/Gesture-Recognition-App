b9320b4a124149b0a651100b75ce659cf22889a7
garc97 <68360711+garc97@users.noreply.github.com>
2024-04-25 20:47:37 -0300

    indicador para cima

--- detection_gesture_app.py
+++ detection_gesture_app.py
@@ -2,13 +2,11 @@ import cv2
 import mediapipe as mp
 import time
 
-
 def inicia_mediapipe():
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
     mp_draw = mp.solutions.drawing_utils
     return hands, mp_draw
-
 
 def processa_frame(img, hands, mp_draw, last_space_press_time, intervalo_tempo):
     img = cv2.flip(img, 1)  # Inverte a imagem
@@ -21,7 +19,6 @@ def processa_frame(img, hands, mp_draw, last_space_press_time, intervalo_tempo):
             finger_positions = get_finger_positions(img, hand_landmarks)
             gesture = identify_gesture(finger_positions)
 
-            # Atualiza a impressão do gesto a cada intervalo de tempo definido
             current_time = time.time()
             if current_time - last_space_press_time >= intervalo_tempo:
                 print(f"Gesture Detected: {gesture} at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
@@ -32,22 +29,24 @@ def processa_frame(img, hands, mp_draw, last_space_press_time, intervalo_tempo):
 
     return img, gesture, last_space_press_time
 
-
 def get_finger_positions(img, hand_landmarks):
     h, w, c = img.shape
     finger_positions = [(id, int(lm.x * w), int(lm.y * h)) for id, lm in enumerate(hand_landmarks.landmark)]
     return finger_positions
 
-
 def identify_gesture(finger_positions):
-    # Verifica se a mão está aberta e a orientação da palma
     if is_hand_open(finger_positions):
         thumb_tip_x = finger_positions[4][1]
         thumb_cmc_x = finger_positions[1][1]
         return "Mao Aberta"
 
+    index_tip = finger_positions[8]
+    index_base = finger_positions[5]
+    other_fingers = [finger_positions[i] for i in [12, 16, 20]]
 
-    # Outras detecções de gestos
+    if index_tip[2] < index_base[2] and all(finger_tip[2] > index_base[2] for finger_tip in other_fingers):
+        return "Indicador Para Cima"
+
     thumb_tip_x = finger_positions[4][1]
     index_tip_x = finger_positions[8][1]
     middle_base_x = finger_positions[9][1]
@@ -65,26 +64,22 @@ def identify_gesture(finger_positions):
 
     return "Nao Definido"
 
-
 def is_hand_open(finger_positions):
-    # Checa se todos os dedos estão estendidos verificando se a ponta está acima da base para cada dedo
     return all(finger_positions[i][2] < finger_positions[i - 3][2] for i in range(8, 21, 4)) and \
-        finger_positions[4][2] < finger_positions[2][2]  # Inclui o polegar
-
+           finger_positions[4][2] < finger_positions[2][2]
 
 def main():
     cap = cv2.VideoCapture(0)
     hands, mp_draw = inicia_mediapipe()
     last_space_press_time = 0
-    intervalo_tempo = 2  # Intervalo em segundos para imprimir o gesto detectado
+    intervalo_tempo = 2
 
     while True:
         success, img = cap.read()
         if not success:
             continue
 
-        img, gesture, last_space_press_time = processa_frame(img, hands, mp_draw, last_space_press_time,
-                                                             intervalo_tempo)
+        img, gesture, last_space_press_time = processa_frame(img, hands, mp_draw, last_space_press_time, intervalo_tempo)
 
         cv2.imshow("Hand Tracking", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -92,7 +87,6 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
