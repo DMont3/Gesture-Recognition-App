c73d12c508482ad6f98b670a64ce5f9548741b2a
DMont3 <daniel.smonteiro@hotmail.com>
2024-05-14 14:22:55 -0300

    Update 2
    Melhora na detecção de gestos, adição de gesto "Duas mãos abertas", gesto "Dois dedos para cima", correção da câmera.

--- detection_gesture_app.py
+++ detection_gesture_app.py
@@ -4,119 +4,142 @@ import time
 import pyautogui
 
 
-
-def inicia_mediapipe():
+def initialize_media_pipe():
     mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
+    hands = mp_hands.Hands(max_num_hands=2, static_image_mode=False, min_detection_confidence=0.5,
+                           min_tracking_confidence=0.5)
     mp_draw = mp.solutions.drawing_utils
     return hands, mp_draw
 
-def processa_frame(img, hands, mp_draw, last_space_press_time, last_pointing_time, intervalo_tempo):
-    img = cv2.flip(img, 1) # Inverte a imagem
+
+def is_hand_open(hand_landmarks):
+    landmarks = hand_landmarks.landmark
+    thumb_tip = landmarks[mp.solutions.hands.HandLandmark.THUMB_TIP]
+    thumb_ip = landmarks[mp.solutions.hands.HandLandmark.THUMB_IP]
+    thumb_mcp = landmarks[mp.solutions.hands.HandLandmark.THUMB_MCP]
+    thumb_cmc = landmarks[mp.solutions.hands.HandLandmark.THUMB_CMC]
+    index_tip = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP]
+    index_mcp = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_MCP]
+    middle_tip = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_TIP]
+    middle_mcp = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_MCP]
+    ring_tip = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_TIP]
+    ring_mcp = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_MCP]
+    pinky_tip = landmarks[mp.solutions.hands.HandLandmark.PINKY_TIP]
+    pinky_mcp = landmarks[mp.solutions.hands.HandLandmark.PINKY_MCP]
+
+    return (thumb_tip.y < thumb_ip.y < thumb_mcp.y < thumb_cmc.y and
+            index_tip.y < index_mcp.y and
+            middle_tip.y < middle_mcp.y and
+            ring_tip.y < ring_mcp.y and
+            pinky_tip.y < pinky_mcp.y)
+
+
+def get_gesture(hand_landmarks, last_action_time, current_time):
+    landmarks = hand_landmarks.landmark
+    thumb_tip = landmarks[mp.solutions.hands.HandLandmark.THUMB_TIP]
+    thumb_ip = landmarks[mp.solutions.hands.HandLandmark.THUMB_IP]
+    thumb_mcp = landmarks[mp.solutions.hands.HandLandmark.THUMB_MCP]
+    thumb_cmc = landmarks[mp.solutions.hands.HandLandmark.THUMB_CMC]
+    index_tip = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP]
+    index_mcp = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_MCP]
+    middle_tip = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_TIP]
+    middle_mcp = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_MCP]
+    ring_tip = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_TIP]
+    ring_mcp = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_MCP]
+    pinky_tip = landmarks[mp.solutions.hands.HandLandmark.PINKY_TIP]
+    pinky_mcp = landmarks[mp.solutions.hands.HandLandmark.PINKY_MCP]
+
+    # Resetar gesto
+    gesto, action_key = "Nenhum Gesto", None
+
+    # Gestos básicos com controle de tempo
+    if is_hand_open(hand_landmarks):
+        if current_time - last_action_time['space'] > 3:
+            last_action_time['space'] = current_time
+            gesto, action_key = "Mão Aberta", 'space'
+    elif index_tip.y < index_mcp.y and all(other.y > index_mcp.y for other in [middle_tip, ring_tip, pinky_tip]):
+        if current_time - last_action_time['up'] > 1:
+            last_action_time['up'] = current_time
+            gesto, action_key = "Indicador Para Cima", 'up'
+    elif index_tip.y > index_mcp.y and all(other.y > index_mcp.y for other in [middle_tip, ring_tip, pinky_tip]):
+        if current_time - last_action_time['down'] > 1:
+            last_action_time['down'] = current_time
+            gesto, action_key = "Indicador Para Baixo", 'down'
+    elif index_tip.y < index_mcp.y and middle_tip.y < middle_mcp.y and all(
+            other.y > middle_mcp.y for other in [ring_tip, pinky_tip]):
+        if current_time - last_action_time['right'] > 1:
+            last_action_time['right'] = current_time
+            gesto, action_key = "Dois Dedos Para Cima", 'right'
+    elif thumb_tip.y < thumb_ip.y < thumb_mcp.y < thumb_cmc.y and pinky_tip.y < pinky_mcp.y and all(
+            tip.y > mcp.y for tip, mcp in [(index_tip, index_mcp), (middle_tip, middle_mcp), (ring_tip, ring_mcp)]):
+        if current_time - last_action_time['left'] > 1:
+            last_action_time['left'] = current_time
+            gesto, action_key = "Hang Loose", 'left'
+    elif index_tip.y < index_mcp.y and pinky_tip.y < pinky_mcp.y and all(
+            other.y > middle_mcp.y for other in [middle_tip, ring_tip]):
+        if current_time - last_action_time['rock'] > 1:
+            last_action_time['rock'] = current_time
+            gesto = "Rock"
+
+    return gesto, action_key
+
+
+def process_frame(img, hands, mp_draw, last_action_time, last_gesture, last_gesture_time):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(img_rgb)
-    gesture = "Nao Definido"
 
+    current_time = time.time()
+    gesto, action_key = "Nenhum Gesto", None
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            finger_positions = get_finger_positions(img, hand_landmarks)
-            gesture = identify_gesture(finger_positions)
-
-            current_time = time.time()
-            if gesture == "Mao Aberta" and current_time - last_space_press_time >= intervalo_tempo:
-                print(f"Gesture Detected: {gesture} at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
-                last_space_press_time = current_time
-                pyautogui.press('space')
-            elif gesture == "Indicador Para Cima":
-                pyautogui.press('up')
-            elif gesture == "Indicador Para Baixo":
-                pyautogui.press('down')
-            elif gesture in ["Apontar Esquerda", "Apontar Direita"] and current_time - last_pointing_time >= 2:
-                last_pointing_time = current_time
-                if gesture == "Apontar Esquerda":
-                    pyautogui.press('left')
-                else:
-                    pyautogui.press('right')
-
-            mp_draw.draw_landmarks(img, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
-            cv2.putText(img, gesture, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
-    return img, gesture, last_space_press_time, last_pointing_time
-
-def get_finger_positions(img, hand_landmarks):
-    h, w, c = img.shape
-    finger_positions = [(id, int(lm.x * w), int(lm.y * h)) for id, lm in enumerate(hand_landmarks.landmark)]
-    return finger_positions
-
-def identify_gesture(finger_positions):
-    if is_hand_open(finger_positions):
-        return "Mao Aberta"
-
-    index_tip = finger_positions[8]
-    middle_tip = finger_positions[12]
-    ring_tip = finger_positions[16]
-    pinky_tip = finger_positions[20]
-    thumb_tip = finger_positions[4]
-    index_base = finger_positions[5]
-    middle_base = finger_positions[9]
-
-    # Detecção do gesto "V de Vitória"
-    if index_tip[2] < index_base[2] and middle_tip[2] < middle_base[2] and \
-       all(finger_tip[2] > middle_base[2] for finger_tip in [ring_tip, pinky_tip]):
-        return "V de Vitoria"
-
-    # Detecção do gesto "Hang Loose"
-    if thumb_tip[2] < finger_positions[2][2] and pinky_tip[2] < finger_positions[17][2] and \
-        index_tip[2] > index_base[2] and middle_tip[2] > middle_base[2] and ring_tip[2] > finger_positions[13][2]:
-        return "Hang Loose"
-
-    # Detecção do gesto "Rock"
-    if index_tip[2] < index_base[2] and pinky_tip[2] < finger_positions[17][2] and \
-       middle_tip[2] > middle_base[2] and ring_tip[2] > middle_base[2]:
-        return "Rock"
-
-    # Detecção do gesto "Indicador Para Cima"
-    if index_tip[2] < index_base[2] and all(finger_tip[2] > index_base[2] for finger_tip in [middle_tip, ring_tip, pinky_tip]):
-        return "Indicador Para Cima"
-
-    # Detecção do gesto "Indicador Para Baixo"
-    if index_tip[2] > index_base[2] and all(finger_tip[2] < index_base[2] for finger_tip in [middle_tip, ring_tip, pinky_tip]):
-    thumb_tip_x = thumb_tip[1]
-    index_tip_x = index_tip[1]
-    middle_base_x = middle_base[1]
-    ring_base_x = ring_tip[1]
-    pinky_base_x = pinky_tip[1]
-
-    if thumb_tip_x < index_tip_x and pinky_tip[2] < ring_tip[2]:
-        return "Apontar Esquerda"
-    elif all(finger_positions[i][2] < finger_positions[i + 4][2] for i in range(5, 17, 4)):
-        average_x = (middle_base_x + ring_base_x + pinky_base_x) / 3
-        if index_tip_x < average_x:
-            return "Legal"
+        if len(results.multi_hand_landmarks) == 2:
+            if all(is_hand_open(hand) for hand in results.multi_hand_landmarks):
+                if current_time - last_action_time['f'] > 3:
+                    last_action_time['f'] = current_time
+                    gesto = "Duas Mãos Abertas"
+                    action_key = 'f'
+                    print(f"Pressing key: {action_key} for gesture: {gesto}")  # Mensagem de depuração
+                    pyautogui.press(action_key)
         else:
-            return "Apontar Direita"
+            for hand_landmarks in results.multi_hand_landmarks:
+                gesto, action_key = get_gesture(hand_landmarks, last_action_time, current_time)
+                mp_draw.draw_landmarks(img, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
+                if action_key:
+                    print(f"Pressing key: {action_key} for gesture: {gesto}")  # Mensagem de depuração
+                    pyautogui.press(action_key)
+                break  # Processar apenas a primeira mão detectada
 
-    return "Nao Definido"
+    if gesto != "Nenhum Gesto":
+        last_gesture = gesto
+        last_gesture_time = current_time
+    elif current_time - last_gesture_time > 1:
+        last_gesture = "Nenhum Gesto"
 
+    print(f"Detected gesture: {last_gesture}")  # Log do gesto detectado
 
-def is_hand_open(finger_positions):
-    return all(finger_positions[i][2] < finger_positions[i - 3][2] for i in range(8, 21, 4)) and \
-           finger_positions[4][2] < finger_positions[2][2]
+    return img, last_gesture, last_gesture_time
+
 
 def main():
     cap = cv2.VideoCapture(0)
-    hands, mp_draw = inicia_mediapipe()
-    last_space_press_time = 0
-    last_pointing_time = 0
-    intervalo_tempo = 5
+    cap.set(3, 640)  # Largura
+    cap.set(4, 480)  # Altura
+    cap.set(cv2.CAP_PROP_FPS, 15)  # Ajustar FPS se necessário
+
+    hands, mp_draw = initialize_media_pipe()
+    last_action_time = {'space': 0, 'up': 0, 'down': 0, 'left': 0, 'right': 0, 'rock': 0, 'f': 0}
+    last_gesture = "Nenhum Gesto"
+    last_gesture_time = time.time()
 
     while True:
         success, img = cap.read()
         if not success:
             continue
 
-        img, gesture, last_space_press_time, last_pointing_time = processa_frame(img, hands, mp_draw, last_space_press_time, last_pointing_time, intervalo_tempo)
+        img, last_gesture, last_gesture_time = process_frame(
+            img, hands, mp_draw, last_action_time, last_gesture, last_gesture_time
+        )
 
+        cv2.putText(img, f'Gesto: {last_gesture}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
         cv2.imshow("Hand Tracking", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -124,5 +147,7 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
     main()
+
